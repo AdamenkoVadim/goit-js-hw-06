71f8bbc5af9b9b9bb40276beb1e0e9856912b54b
AdamenkoVadim <106696623+AdamenkoVadim@users.noreply.github.com>
2023-01-15 20:26:30 +0100

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,21 +1,10 @@
 const categoriesEl = document.querySelectorAll('li.item');
-console.log(categoriesEl);
 console.log(`Number of categories: ${categoriesEl.length}`);
+console.log('');
 
-const ul = [];
-
-for (const count of categoriesEl) {
-    console.log(count);
-    console.log('H',count.firstElementChild.textContent);
-    console.log('UL',count.lastElementChild);
-    ul.push(count.firstElementChild.textContent);
-
-}
-
-
-const nameCtegory = categoriesEl.forEach(name =>{
-    console.log(name.firstElementChild.textContent);
-    console.log(name.lastElementChild.textContent);
-})
-
+const categories = categoriesEl.forEach(name => {
+    console.log(`Category: ${name.firstElementChild.textContent}`);
+    console.log(`Elements: ${name.children[1].children.length}`);
+    console.log('');
+    });
 
