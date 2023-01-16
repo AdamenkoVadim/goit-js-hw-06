23e584cc8ae39acc12e922bcaf504ad973e4cbd4
AdamenkoVadim <106696623+AdamenkoVadim@users.noreply.github.com>
2023-01-16 21:43:17 +0100

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,30 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const amount = document.querySelector('input');
+const boxes = document.querySelector('[id="boxes"]');
+
+createBtn.addEventListener('click',()=>{
+  boxes.innerHTML = '';
+  const boxSize = 30;
+  for (let i = 0; i < amount.value; i++) {
+    const num = boxSize + i * 10;
+    console.log(i);
+    const divEl = document.createElement('div');
+    divEl.style.cssText =`width: ${num}px;
+    height: ${num}px;
+    background-color: ${getRandomHexColor()};`
+    boxes.appendChild(divEl);
+  }
+});
+
+destroyBtn.addEventListener('click',destroyBoxes);
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
+}
+  
+
