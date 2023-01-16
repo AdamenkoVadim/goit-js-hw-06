9e005ebb06a63fdc62e1e99778e305ad44ac3daf
AdamenkoVadim <106696623+AdamenkoVadim@users.noreply.github.com>
2023-01-16 21:43:03 +0100

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,17 @@
+
+
+const inputEl = document.querySelector('[id="validation-input"]');
+
+const lengthEL = document.querySelector('[data-length]')
+
+
+inputEl.addEventListener('blur', (event) => {
+    if(event.currentTarget.value.length === inputEl.dataset.length) {
+        inputEl.classList.add('valid');
+        inputEl.classList.remove('invalid')
+    } else {
+        inputEl.classList.add('invalid');
+        inputEl.classList.remove('valid');
+
+    }
+} );
