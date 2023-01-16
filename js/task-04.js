d849734a9d3c33c8b5bd4f0d0b2d1ee9975bd70c
AdamenkoVadim <106696623+AdamenkoVadim@users.noreply.github.com>
2023-01-16 16:30:49 +0100

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,24 @@
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const valueEl = document.querySelector('[id="value"]');
+
+const counter = {
+    value: 0,
+    decrement() {
+        this.value -= 1;
+    },
+    increment() {
+        this.value += 1;
+    },
+};
+
+decrementBtn.addEventListener('click',() => {
+    counter.decrement();
+    valueEl.textContent = counter.value;
+});
+
+incrementBtn.addEventListener('click', () => {
+    counter.increment();
+    valueEl.textContent = counter.value;
+});
+
