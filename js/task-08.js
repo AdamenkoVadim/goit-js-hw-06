ed17dff824a037dddb62384d08cb6ea10f005892
AdamenkoVadim <106696623+AdamenkoVadim@users.noreply.github.com>
2023-01-16 21:43:09 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const formEl = document.querySelector('.login-form');
+formEl.addEventListener('submit', (event) => {
+   event.preventDefault();
+   const {elements:{email,password}} = event.currentTarget;
+
+   if(email.value ==='' || password.value === '') {
+    alert('All fields must be filled')
+   } else {
+    console.log({'email':email.value, 'password': password.value});
+    event.currentTarget.reset();
+   }
+
+})
+
 
