bc5d12cb705667fa1476adba61f28161561e0566
AdamenkoVadim <106696623+AdamenkoVadim@users.noreply.github.com>
2023-01-16 21:43:15 +0100

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeBtn = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+const colorSpan = document.querySelector('.color');
+
+changeBtn.addEventListener('click', () => {
+  const random = getRandomHexColor();
+  bodyEl.style.backgroundColor = random;
+  colorSpan.textContent = random;
+  console.log(random);
+})
